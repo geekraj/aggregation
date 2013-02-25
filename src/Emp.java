b1ad14bf36dda557faaa9e6d8bb6c0b263495636
geeklabs <com.gklabs@gmail.com>
2013-02-25 17:12:12 +0530

    first commit

---
+++ src/Emp.java
@@ -0,0 +1,16 @@
+public class Emp {
+
+;
+	public static void main(String[] args) {
+		String a = new String("geek");
+		String b = new String("geek");
+		if (a == b) {
+			System.out.println("true");
+		}
+
+		else 
+			System.out.println("false");
+		
+
+	}
+}
